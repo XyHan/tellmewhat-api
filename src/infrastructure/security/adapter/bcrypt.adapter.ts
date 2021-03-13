59ffb932b8d05e2c7507166f59d5c73ca0e0fde8
XyHan <xyhan_1@hotmail.com>
2021-03-13 17:32:21 +0100

    feat(adapter): add bcrypt adapter

---
+++ src/infrastructure/security/adapter/bcrypt.adapter.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { EncrypterInterface } from '../../../domain/utils/encrypter.interface';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class BcryptAdapter implements EncrypterInterface {
+  public async compare(plainPassword: string, hashedPassword: string): Promise<boolean>  {
+    return await bcrypt.compare(plainPassword, hashedPassword);
+  }
+
+  public async hash(plainPassword: string, salt: string): Promise<string>  {
+    return await bcrypt.hash(plainPassword, salt);
+  }
+
+  public async salt(): Promise<string> {
+    return await bcrypt.genSalt(10);
+  }
+}
